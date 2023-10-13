6af3d3d2f10f70a9cec04ad648bb44d5e78101f9
Johnmarc22 <146808774+Johnmarc22@users.noreply.github.com>
2023-10-13 21:05:03 +0800

    Add files via upload

---
+++ act7.js
@@ -0,0 +1,6 @@
+   let password = "26jhno"; 
+ if ( password.length >= 8 &&  password.includes("p@ss")){ 
+         console.log("Valid password"); 
+ } else{ 
+     console.log("Invalid password"); 
+ }
