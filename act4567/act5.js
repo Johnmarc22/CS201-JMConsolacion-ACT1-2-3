9a0376cc7ed9a00e1c0f3d82c7e53e750fa814a7
Johnmarc22 <146808774+Johnmarc22@users.noreply.github.com>
2023-10-13 21:11:05 +0800

    Delete act4567 directory

--- act4567/act5.js
+++
@@ -1,3 +0,0 @@
-for (let i = 0; i < 1001; i++) {
-    console.log("Quitasol, Kenlei A.", i);
-  }
--- act4567/act6.js
+++
@@ -1,5 +0,0 @@
-var num=10;
-while (num < 110){
-   console.log("Quitasol, Kenlei A.", num);
-   num++;
-}
