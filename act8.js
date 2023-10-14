81ad8673daa3f9d5d819d295a24d9716634a0cb2
Johnmarc22 <146808774+Johnmarc22@users.noreply.github.com>
2023-10-14 09:23:02 +0800

    Add files via upload

---
+++ act8.js
@@ -0,0 +1,17 @@
+const favSingers = ["Brad Paisley", "Lany", "Post Malone "];   
+  console.log (favSingers);   
+  const favNumbers = ["26","5","11","14"];   
+  console.log (favNumbers);   
+  const mixedArr = ["string",["otherarray"],123,true];   
+  
+  console.log (favSingers[0]);   
+  console.log (favSingers[1]);   
+  console.log (favSingers[2]);   
+  console.log (favNumbers[0]);   
+  console.log (favNumbers[1]);   
+  console.log (favNumbers[2]);   
+  console.log (favNumbers[3]);   
+  console.log (mixedArr[0]);   
+  console.log (mixedArr[1]);   
+  console.log (mixedArr[2]);   
+  console.log (mixedArr[3]);
